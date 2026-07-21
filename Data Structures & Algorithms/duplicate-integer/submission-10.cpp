89b7bb23bb0af90a0337c925baf6231a8f5fac4b
vankudothsunilkumar54-alt <vankudothsunilkumar54@gmail.com>
2026-07-21 11:45:30 +0530

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+       // bool ok=false;
+        int n=nums.size();
+       unordered_set<int>s;
+       for(int i=0;i<n;i++){
+        if(s.find(nums[i])!=s.end()) return true;
+        else s.insert(nums[i]);
+       }
+       return false;
+    }
+};
